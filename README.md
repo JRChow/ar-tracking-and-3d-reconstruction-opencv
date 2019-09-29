e2a5cd4798fec935cf28ffe5f8c2dce886e71179
Jerome <zjrtomjohn@hotmail.com>
2019-09-29 16:33:42 -0700

    Calibrated mobile camera

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ar-tracking-and-3d-reconstruction-opencv
+
+
+## Mobile
+
+- Image scaled to 0.25 of original size to speed up computation
---
+++ mobile/run_calib.py
@@ -0,0 +1,4 @@
+import CalibrationHelpers as calib
+intrinsics, distortion, roi, new_intrinsics = \
+    calib.CalibrateCamera('calibration_data', True)
+calib.SaveCalibrationData('calibration_data', intrinsics, distortion, new_intrinsics, roi)
